a1dc073c12153bc55be330e5cfaff03dd6b2bfe6
dflorian251 <inf2021044@ionio.gr>
2024-04-19 13:14:56 +0300

    Footer section segmentation and addition

--- admin/pages/events/admin_approve_event.php
+++ admin/pages/events/admin_approve_event.php
@@ -41,7 +41,7 @@
         <div class="main-panel">
           <div class="content-wrapper">
             <div class="page-header">
-              <h3>Adevrt Approving </h3>
+              <h3>Advert Approving </h3>
             </div>
             <form class="nice-form-group" action="../../admin_operations/approve_event.php" method="post">
               <label for="event_id">Adverts:</label>
@@ -74,12 +74,9 @@
           </div>
           <!-- content-wrapper ends -->
           <!-- partial:../../partials/_footer.html -->
-          <footer class="footer">
-            <div class="container-fluid d-flex justify-content-between">
-              <span class="text-muted d-block text-center text-sm-start d-sm-inline-block">Copyright © bootstrapdash.com 2021</span>
-              <span class="float-none float-sm-end mt-1 mt-sm-0 text-end"> Free <a href="https://www.bootstrapdash.com/bootstrap-admin-template/" target="_blank">Bootstrap admin template</a> from Bootstrapdash.com</span>
-            </div>
-          </footer>
+          <?php
+            include "../../footer.html";          
+          ?>
           <!-- partial -->
         </div>
         <!-- main-panel ends -->
